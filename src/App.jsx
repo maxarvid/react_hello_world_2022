6c9fd8709440d400a5131296d7db669d8631bdd8
maxarvid <max.arvid.anderson@gmail.com>
2022-02-07 15:32:04 +0100

    Adds application state to class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     planet: 'Venus'
+  //   }
+  // }
+
+  state = {
+    planet: "Venus",
+    whatever: "LV-426"
+  };
+
   render() {
-    return <div>
-      <h1>Hello World from class component</h1>
-    </div>;
+    // const planet = "Venus";
+    const { planet } = this.state;
+    return (
+      <div>
+        <h1>Hello {planet} from class component</h1>
+      </div>
+    );
   }
 }
 
