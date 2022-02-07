9a1c3a5e718111f7f646d6f88f5620984d07719c
maxarvid <max.arvid.anderson@gmail.com>
2022-02-07 15:22:35 +0100

    cleans react app adds hello world class component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return <div>
+      <h1>Hello World from class component</h1>
+    </div>;
+  }
+}
+
+export default App;
